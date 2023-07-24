102de2070f573e7e44fa572d2f6f49657d6cc239
SirBeho <benjamin.tavarez.98@gmail.com>
2023-07-24 18:46:27 -0400

    Datos de prueva

--- php/connection.php
+++ php/connection.php
@@ -1,8 +1,7 @@
 <?php
 try {
     $mysqli = new mysqli("localhost", "root", "", "autentificacion");
-    echo "Coneccion exitosa";
-
+    
 } catch (mysqli_sql_exception $e) {
     echo "Error:". $e->getMessage();
 }
--- php/update.php
+++ php/update.php
@@ -9,35 +9,27 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
  
     $user_id= $_SESSION['usuario']['id'];
 
-    
-
     $file_tmp = $_FILES["profile_photo"]["tmp_name"];
     $file_error = $_FILES["profile_photo"]["error"];
 
-    $file_name = $file_error == 0 ? "photo_".$user_id : null;
-    $phone = $phone == 0 ? null : $phone;
-    var_dump($_FILES["profile_photo"]);
+    $file_name = ($_SESSION['usuario']['img'] == null and $file_error == 4) ?  null : "photo_".$user_id;
+      
      $query = "UPDATE usuario SET name='$name', bio='$bio', phone='$phone', email='$email' ,img ='$file_name' WHERE id='$user_id'";
     
-    if ($mysqli->query($query) === true and $file_error === 0) {
-    
-        move_uploaded_file($file_tmp, "../pictures/".$file_name);
+    if ($mysqli->query($query) === true and (!$file_error or $file_error == 4)) {
+        
+        if(!$file_error){
+            move_uploaded_file($file_tmp, "../pictures/".$file_name);
+        }
        
-
-
         $resultado = $mysqli -> query("select * from usuario where id = '$user_id'");
         $_SESSION['usuario'] = $resultado-> fetch_assoc();
-
         $_SESSION['success_message'] = "Datos actualizados correctamente";
         header("Location: ../profile.php"); 
         exit;
-    } else {
-        if($file_error === 0){
-            $_SESSION['error_message'] = "Error al actualizar los datos: " . $mysqli->error;
-        }else{
-            $_SESSION['error_message'] = Error_File($file_error);
-        }
-        header("Location: ../profile_edit.php"); 
+    }else {
+        $_SESSION['error_message'] = ($file_error === 0 or $file_error === 4 ) ? "Error al actualizar los datos: " . $mysqli->error : Error_File($file_error);
+        header("Location: ../profile_edit.php");
         exit;
     } 
 };
